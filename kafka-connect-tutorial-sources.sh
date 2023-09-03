02f5410e53313e36c613db1b676913b885c4c2ea
DriRSantos <drirsantos@outlook.com>
2023-09-03 19:27:16 -0300

    kafka connect source, wikimedia-recentchange-connect distributed mode

--- kafka-connect-tutorial-sources.sh
+++ kafka-connect-tutorial-sources.sh
@@ -58,12 +58,16 @@ kafka-console-consumer --topic demo-2-distributed --from-beginning --bootstrap-s
 ###############
 
 ###############
-# C) TwitterSourceConnector in distributed mode:
-# create the topic we're going to write to
-docker run --rm -it --net=host landoop/fast-data-dev:cp3.3.0 bash
-kafka-topics --create --topic demo-3-twitter --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+# C) TwitterSourceConnector in distributed mode (USED WikimediaSourceConnector instead):
+# create the topic we're going to write to (created wikimedia.recent.change)
+# docker run --rm -it --net=host landoop/fast-data-dev:cp3.3.0 bash
+docker run --rm -it --net=host landoop/fast-data-dev:latest bash
+# kafka-topics --create --topic demo-3-twitter --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+kafka-topics --create --topic wikimedia.recentchange.connect --partitions 3 --replication-factor 1 --bootstrap-server 127.0.0.1:9092
+
 # Start a console consumer on that topic
 kafka-console-consumer --topic demo-3-twitter --bootstrap-server 127.0.0.1:9092
+kafka-console-consumer --topic wikimedia.recentchange.connect --bootstrap-server 127.0.0.1:9092
 
 # Follow the instructions at: https://github.com/Eneco/kafka-connect-twitter#creating-a-twitter-application
 # To obtain the required keys, visit https://apps.twitter.com/ and Create a New App. Fill in an application name & description & web site and accept the developer aggreement. Click on Create my access token and populate a file twitter-source.properties with consumer key & secret and the access token & token secret using the example file to begin with.
